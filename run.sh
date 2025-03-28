012c27c5a70bb3af57489e195ab7e74823b106e7
WT3210 <weitseng321@gmail.com>
2025-03-28 14:39:09 +0800

    auto: update code on 2025-03-28 14:39:09

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-echo '🟢 啟動錄音 + GPS 紀錄'
+echo "🟢 啟動錄音 + GPS 紀錄"
+
+# 啟動 GPSD（每次強制重啟）
+echo "🛰 啟動 GPS 服務..."
+sudo killall gpsd 2>/dev/null
+sudo gpsd /dev/serial0 -F /var/run/gpsd.sock
+
+# 開始錄音 + GPX
 python3 record_with_gps.py
